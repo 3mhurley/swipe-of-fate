54edde42b8a361de806e892f7af263e0cc869e3a
Evan Hurley <3mhurley@gmail.com>
2025-01-08 23:34:17 -0700

    initialize main scene

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,3 +1,4 @@
+@tool
 class_name Enemy extends Node2D
 
 @export var enemy_name: String = "Enemy Name"
@@ -17,9 +18,14 @@ func set_enemy_values(_name: String, _health: int):
 	enemy_health = _health
 	#enemy_image_sprite = _sprite
 
-	enemy_name_label.set_text(_name)
-	enemy_health_label.set_text(str(_health))
+	_update_graphics()
+
+func _update_graphics():
+	if enemy_name_label.get_text() != enemy_name:
+		enemy_name_label.set_text(enemy_name)
+	if enemy_health_label.get_text() != str(enemy_health):
+		enemy_health_label.set_text(str(enemy_health))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(_delta: float) -> void:
+	_update_graphics()
--- Scripts/tunnel.gd
+++ Scripts/tunnel.gd
@@ -7,14 +7,13 @@ extends Node2D
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
 
 func _on_button_pressed() -> void:
 	var enemy: Enemy = enemy_scene.instantiate()
 	spawn_point.add_child(enemy)
 	enemy.set_enemy_values("Knight", 10)
 	enemy.visible = true
+
