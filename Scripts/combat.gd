f901134f72f210db23461a50545c3106466c49c4
Evan Hurley <3mhurley@gmail.com>
2025-01-21 22:30:48 -0700

    adding end screen wip

--- Scripts/combat.gd
+++ Scripts/combat.gd
@@ -3,6 +3,7 @@ extends Node2D
 @export var key_name: String = "block_right"
 @export var damage_image_rotation: String = "Left"
 @export var parriable: bool = false
+@export var kill_count: int = 0
 
 @onready var enemy_scene: PackedScene = preload("res://Sprites/enemy.tscn")
 @onready var enemy: Enemy = enemy_scene.instantiate()
@@ -44,6 +45,18 @@ func damage_opponent() -> void:
 		get_node("CombatTimer").stop()
 		opponent_node.get_node("Enemy").queue_free()
 		opponent_node.get_node("Damage").queue_free()
+		# remove_child(opponent_node.get_node("Enemy"))
+		# remove_child(opponent_node.get_node("Damage"))
+
+		for i in opponent_node.get_children():
+			print(i.name)
+
+		# if kill_count < 3:
+		# 	kill_count += 1
+		# 	start_game()
+		# else:
+		# 	get_parent().get_node("CanvasLayer/WinScreen").visible = true
+
 
 func get_direction(start_pos: Vector2, end_pos: Vector2) -> String:
 	var direction_vector: Vector2
@@ -64,9 +77,16 @@ func get_direction(start_pos: Vector2, end_pos: Vector2) -> String:
 	return "None"  # No significant movement in either axis
 
 
+func start_game() -> void:
+	create_enemy()
+	create_damage()
+	get_node("CombatTimer").wait_time = 2
+	get_node("CombatTimer").start()
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	get_node("CombatTimer").wait_time = 2
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -96,6 +116,7 @@ func _input(event: InputEvent) -> void:
 					parriable = false
 					get_node("ParryingSFX").play()
 					damage_opponent()
+					get_node("CombatTimer").wait_time = rng.randi_range(1, 5)
 
 
 func _on_timer_timeout() -> void:
@@ -121,12 +142,10 @@ func _on_timer_timeout() -> void:
 		damage_sprite.visible = false
 		parriable = false
 		damage_player()
-	
-	get_node("CombatTimer").wait_time = rng.randi_range(1, 5)
+		get_node("CombatTimer").wait_time = rng.randi_range(1, 5)
 
 
 func _on_button_pressed() -> void:
-	get_node("CombatTimer").start()
 	get_parent().get_node("CanvasLayer/StartButton").visible = false
-	create_enemy()
-	create_damage()
+
+	start_game()
